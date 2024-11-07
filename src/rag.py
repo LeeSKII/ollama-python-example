455fc9b84e261fb26492b57f0bcd1c8c00412c16
DESKTOP-TJ325BT\Administrator <leeskiikseel@outlook.com>
2024-11-07 11:00:56 +0800

    update rag

---
+++ src/rag.py
@@ -0,0 +1,45 @@
+import ollama
+import chromadb
+
+documents = [
+  "lee是一名著名运动员",
+  "dell电脑TX675使用intel core i7处理器并且拥有16gb内存，1tb硬盘",
+]
+
+client = chromadb.Client()
+collection = client.create_collection(name="docs")
+
+# store each document in a vector embedding database
+for i, d in enumerate(documents):
+  response = ollama.embeddings(model="mxbai-embed-large", prompt=d)
+  embedding = response["embedding"]
+  collection.add(
+    ids=[str(i)],
+    embeddings=[embedding],
+    documents=[d]
+  )
+  
+  
+  # an example prompt
+prompt = "请告知我一些关于dell电脑TX675的情况"
+
+# generate an embedding for the prompt and retrieve the most relevant doc
+response = ollama.embeddings(
+  prompt=prompt,
+  model="mxbai-embed-large"
+)
+results = collection.query(
+  query_embeddings=[response["embedding"]],
+  n_results=1
+)
+data = results['documents'][0][0]
+
+print(f"The most relevant document to the prompt '{prompt}' is: {results}")
+
+# generate a response combining the prompt and data we retrieved in step 2
+output = ollama.generate(
+  model="qwen2.5:7b",
+  prompt=f"Using this data: {data}. Respond to this prompt: {prompt}"
+)
+
+print(output['response'])
